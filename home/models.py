e2ad7440f11414845521a2291d21885772faf79d
rcyberly <151371600+rcyberly@users.noreply.github.com>
2025-08-07 20:35:20 +0530

    22469

--- home/models.py
+++ home/models.py
@@ -5,6 +5,10 @@ class Restaurant(object):
     def'__init__'(self, name = "THE NORTSOUT RESTAURANT", slogan = "TASTE CONNECTS FROM NORTH TO SOUTH"):
         self.name = "THE NORTSOUT RESTAURANT"
         self.slogan = "TASTE CONNECTS FROM NORTH TO SOUTH"
-    def save(self):
+    def save(self,instance):
         f{"SAVING RESTAURANT NAME '{self.name}' TO DATABASE "}
+        restaurant_instance = Restaurant.name("THE NORTSOUT RESTAURANT")
+        restaurant_instance.save()
+
+
 
