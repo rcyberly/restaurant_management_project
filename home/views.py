a20cc1c8c4a32f681919687a92135ce05260903b
rcyberly <151371600+rcyberly@users.noreply.github.com>
2025-08-07 20:38:20 +0530

    22469

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, request
+from django.db import models
 
 # Create your views here.
+def homepageview (request):
+    try:
+        
+    return(request, homepageview, context)   
