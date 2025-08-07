0e09d6b0c8387f3523b6713682555cf1627d67f1
rcyberly <151371600+rcyberly@users.noreply.github.com>
2025-08-07 20:29:06 +0530

    22469

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(object):
+    def'__init__'(self, name = "THE NORTSOUT RESTAURANT", slogan = "TASTE CONNECTS FROM NORTH TO SOUTH"):
+        self.name = "THE NORTSOUT RESTAURANT"
+        self.slogan = "TASTE CONNECTS FROM NORTH TO SOUTH"
+    def save(self):
+        f{"SAVING RESTAURANT NAME '{self.name}' TO DATABASE "}
+
