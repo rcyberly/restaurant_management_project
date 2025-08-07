e4ab7daf89953f0be027d0ee3ee7ef53cfc5f14a
rcyberly <151371600+rcyberly@users.noreply.github.com>
2025-08-07 20:44:38 +0530

    22469

--- home/views.py
+++ home/views.py
@@ -4,5 +4,11 @@ from django.db import models
 # Create your views here.
 def homepageview (request):
     try:
+        Restaurant_from_db = Restaurant.objects,first()
+        Restaurnat_name = Restaurant.objects.name()
+        Restaurant_slogan = Restaurant.objects.slogan()
+    except:
+        Restaurnat_name = Restaurnat_name
+        Restaurant_slogan = 
         
     return(request, homepageview, context)   
