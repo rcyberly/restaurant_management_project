fb96ae533e41bb42be105df8a7f5ff715e4f77d5
rcyberly <151371600+rcyberly@users.noreply.github.com>
2025-08-09 20:28:31 +0530

    26112

--- views.py
+++ views.py
@@ -2,4 +2,13 @@ from django.conf import settings
 from django.shortcuts import render
 
 def contact_number(request):
+    PHONENUMBER = 'PHONENUMBER',
+    API_KEY -'APIKEY',
+    DEBUG = True
+    context = {
+        'PHONENUMBER': PHONENUMBER,
+        'API_KEY' : API_KEY,
+        'DEBUG': True
+    }
+    request render(request, 'app/homepage.html', context)
     
