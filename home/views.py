65720f45b1e3927b82489b8ce65a60e08a95cc3c
rcyberly <151371600+rcyberly@users.noreply.github.com>
2025-08-13 19:50:00 +0530

    29812

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, request
 from django.db import models
+from django.contrib import messages
+
 
 # Create your views here.
 def homepageview (request):
@@ -21,5 +23,24 @@ def homepageview (request):
     return(request, homepageview, context)   
 
     def reservation_page_view(request):
-        viewsets = reservations.objects.all()
+        if request.method =='POST':
+            form = reservation_page_view(request.POST)
+            if form_is_valid():
+            try:
+                context = {
+                    'name':name,
+                    'email':email,
+                    'contactno':contactno,
+                }
+                form_reservations.save()
+                message.success(request,'your form has been saved')
+            except:
+                if error:
+                    message.error(request,'there may have been some error ')
+        else:
+            form = reservation_page_view()
+        return render(request, 'home/Reservation_page.html', context)
+                
+
+        
         
