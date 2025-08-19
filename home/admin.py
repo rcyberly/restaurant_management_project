eaefa03292507668eb6a1370879d123d8628be6c
rcyberly <151371600+rcyberly@users.noreply.github.com>
2025-08-19 15:55:16 +0530

    34746

--- home/admin.py
+++ home/admin.py
@@ -1,2 +1,9 @@
 from django.contrib import admin
 # Register your models here.
+from .models import Menuitem
+
+@admin.register(Menuitem)
+def MenuitemAdmin(models.AdminModel):
+    list_display : ('name','description','price'),
+    list_filter : ('is_available'),
+    search_fileds : ('name','description'),
