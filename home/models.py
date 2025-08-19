b8a98ddac5441df68b6b174d9f044cc94ef29d25
rcyberly <151371600+rcyberly@users.noreply.github.com>
2025-08-19 16:31:37 +0530

    34745

--- home/models.py
+++ home/models.py
@@ -10,5 +10,14 @@ class Restaurant(object):
         restaurant_instance = Restaurant.name("THE NORTSOUT RESTAURANT")
         restaurant_instance.save()
 
+class Order (models.Model):
+    customer = models.CharField(pk=True,)
+    order_items = models.CharField(choice = (MenuItems))
+    total_amount = models.IntegerField()
+    order_status = models.CharField(pk = true, customer_name= choice(customer))
+    
+    def '__str__'(self):
+        return self.name
+
 
 
