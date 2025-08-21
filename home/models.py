a3ead35cb858d217676875f08cee7d94476b3119
rcyberly <151371600+rcyberly@users.noreply.github.com>
2025-08-21 15:29:11 +0530

    36182

--- home/models.py
+++ home/models.py
@@ -36,6 +36,11 @@ class Contact(models.Model):
     name = models.CharField(max_length= 80)
     email = models.CharField(max_length = 100)
 
+class Menuitem (models.Model):
+    name = models.CharField(max_length = 50)
+    description = models.TextField()
+    price = models.DecimalField(max_digits = 6, decimal_places = 2)
+
 class MenuItems(models.Model):
     item_name = models.CharField(max_length=30)
     item_description = models.TextField()
