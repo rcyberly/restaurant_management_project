2646b52d7bff9a08c53ac1a060ad0ca9497c7147
rcyberly <151371600+rcyberly@users.noreply.github.com>
2025-08-23 14:53:21 +0530

    37322

---
+++ home/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+Class ContactForms(forms.Form):
+    name = forms.CharField(max_length = 50)
+    email = forms.EmailField()
+    subject = forms.CharField(max_length=200)
+    message = forms.CharField(widget = forms.TextArea)
