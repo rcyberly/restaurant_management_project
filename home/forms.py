5157a16ab0928cb737ebbe1abb4b2ddac8fcbd1a
rcyberly <151371600+rcyberly@users.noreply.github.com>
2025-08-26 22:45:50 +0530

    38677

--- home/forms.py
+++ home/forms.py
@@ -5,3 +5,7 @@ Class ContactForms(forms.Form):
     email = forms.EmailField()
     subject = forms.CharField(max_length=200)
     message = forms.CharField(widget = forms.TextArea)
+
+class ContactFormsOne(form.Form):
+    email = forms.EmailField()
+    message = forms.CharField(widget = forms.TextArea)
